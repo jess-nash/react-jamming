d84b17b212302c875d6ecba725a49e0a8afc596d
Jessica Nash <jessicanashacc@gmail.com>
2023-01-30 12:39:09 +0900

    import track and tracklist css

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Track.css';
 
 class Track extends React.Component {
   renderAction() {
--- src/Components/TrackList/Tracklist.js
+++ src/Components/TrackList/Tracklist.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './TrackList.css';
 import Track from '../Track/Track';
 
 class TrackList extends React.Component {
