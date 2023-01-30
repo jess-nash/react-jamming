eb8fad4a434b0c3e15bcb8bd71ac3142b02feee9
Jessica Nash <jessicanashacc@gmail.com>
2023-01-30 15:18:45 +0900

    obtain spotify access token

---
+++ src/util/Spotify.js
@@ -0,0 +1,31 @@
+let accessToken = ''
+
+const clientID = '11227a3b84544da493f9b1efc2813301';
+const redirectURI = 'http://localhost:3000/';
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+
+    //check for access token match
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+
+      // this will clear the parameters, allowing us to grab a new access token when it expires.
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
+    } else {
+      const accessURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`
+      window.location = accessURL;
+    }
+  }
+}
+
+export default Spotify
