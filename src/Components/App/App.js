767c762657743418ff5b678673776ab875fe3846
Jessica Nash <jessicanashacc@gmail.com>
2023-01-30 12:37:22 +0900

    fix hard coded playlist objects

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -14,9 +14,18 @@ class App extends React.Component {
         { name: 'name2', artist: 'artist2', album: 'album2', id: 2},
         { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }],
       playlistName: 'my playlist',
-      playlistTracks: [{ name: 'name1', artist: 'artist1', album: 'album1', id: 1 },
-        { name: 'name2', artist: 'artist2', album: 'album2', id: 2}]
+      playlistTracks: [{ name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4 },
+        { name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5},
+        { name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6 }]
     };
+  }
+
+  addTrack(track) {
+    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    } else {
+
+    }
   }
 
   render() {
