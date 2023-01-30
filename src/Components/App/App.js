7d4f2617a91c8a001c8b862e124640f7f797802f
Jessica Nash <jessicanashacc@gmail.com>
2023-01-30 13:14:49 +0900

    can remove tracks from a playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -20,6 +20,7 @@ class App extends React.Component {
     };
 
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
@@ -28,6 +29,13 @@ class App extends React.Component {
       return;
     }
     tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
     this.setState({playlistTracks: tracks});
   }
 
@@ -44,7 +52,7 @@ class App extends React.Component {
             <Playlist
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
-            />
+              onRemove={this.removeTrack}/>
           </div>
         </div>
       </div>
--- src/Components/TrackList/Tracklist.js
+++ src/Components/TrackList/Tracklist.js
@@ -11,7 +11,9 @@ class TrackList extends React.Component {
             return <Track
               track={track}
               key={track.id}
-              onAdd={this.props.onAdd}/>
+              onAdd={this.props.onAdd}
+              onRemove={this.props.onRemove}
+              isRemoval={this.props.isRemoval}/>
           })
         }
       </div>
