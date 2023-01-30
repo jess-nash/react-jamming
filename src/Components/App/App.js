d4ba59aade40fdcf415f4aa2d4e4651c68e78d7b
Jessica Nash <jessicanashacc@gmail.com>
2023-01-30 11:06:16 +0900

    change app from function to class comp

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,21 +3,24 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar.js'
 import SearchResults from '../SearchResults/SearchResults.js'
 import Playlist from '../Playlist/Playlist.js'
+import React from 'react';
 
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <Playlist />
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
